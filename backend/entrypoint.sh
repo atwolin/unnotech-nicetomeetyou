3fffb75367272e376a22ff784a12174042f7f6a2
atwolin@linux <atwolin@gmail.com>
2026-02-27 12:26:18 +0800

    feat: updated the backend service to depend on the new PostgreSQL service.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Wait for the database to be available
+/app/wait-for-it.sh db:5432 -- echo "DB is ready"
+
 # python manage.py migrate --noinput
 
 # Collect static files for serving via WhiteNoise/gunicorn
